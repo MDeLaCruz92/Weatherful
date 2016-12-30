4e6d5467965e1c4d7fa691428c3c544fc67fa3f4
MDeLaCruz92 <delacruzm10@gmail.com>
2016-12-29 23:04:33 -0500

    Updated Files.

--- MiWeather/CurrentWeather.swift
+++ MiWeather/CurrentWeather.swift
@@ -10,88 +10,88 @@ import Alamofire
 import CoreLocation
 
 struct URLs {
-    var coordinates:CLLocationCoordinate2D?
-    init(withCoordinate coordinate:CLLocationCoordinate2D){
-        self.coordinates = coordinate
-    }
-    
-    var getWeatherUrl:String {
-        guard let cord = self.coordinates else { fatalError() }
-        return  "http://api.openweathermap.org/data/2.5/weather?lat=\(cord.latitude)&lon=\(cord.longitude)&appid=0c478effb593bd383336c8191b18e20d"
-    }
-    
-    var getForcastUrl:String   {
-        guard let cord = self.coordinates else { fatalError() }
-        return "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(cord.latitude)&lon=\(cord.longitude)&cnt=10&mode=json&appid=0c478effb593bd383336c8191b18e20d"
-    }
+  var coordinates:CLLocationCoordinate2D?
+  init(withCoordinate coordinate:CLLocationCoordinate2D){
+    self.coordinates = coordinate
+  }
+  
+  var getWeatherUrl:String {
+    guard let cord = self.coordinates else { fatalError() }
+    return  "http://api.openweathermap.org/data/2.5/weather?lat=\(cord.latitude)&lon=\(cord.longitude)&appid=0c478effb593bd383336c8191b18e20d"
+  }
+  
+  var getForcastUrl:String   {
+    guard let cord = self.coordinates else { fatalError() }
+    return "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(cord.latitude)&lon=\(cord.longitude)&cnt=10&mode=json&appid=0c478effb593bd383336c8191b18e20d"
+  }
 }
 
 class CurrentWeather {
-    var _cityName: String!
-    var _date: String!
-    var _weatherType: String!
-    var _currentTemp: Double!
-    
-    var cityName: String {
-        if _cityName == nil {
-            _cityName = ""
-        }
-        return _cityName
+  var _cityName: String!
+  var _date: String!
+  var _weatherType: String!
+  var _currentTemp: Double!
+  
+  var cityName: String {
+    if _cityName == nil {
+      _cityName = ""
+    }
+    return _cityName
+  }
+  
+  var date: String {
+    if _date == nil {
+      _date = ""
     }
     
-    var date: String {
-        if _date == nil {
-            _date = ""
-        }
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .long
-        dateFormatter.timeStyle = .none
-        let currentDate = dateFormatter.string(from: Date())
-        self._date = "Today, \(currentDate)"
-        return _date
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .long
+    dateFormatter.timeStyle = .none
+    let currentDate = dateFormatter.string(from: Date())
+    self._date = "Today, \(currentDate)"
+    return _date
+  }
+  
+  var weatherType: String {
+    if _weatherType == nil {
+      _weatherType = ""
     }
-    
-    var weatherType: String {
-        if _weatherType == nil {
-            _weatherType = ""
-        }
-        return _weatherType
+    return _weatherType
+  }
+  
+  var currentTemp: Double {
+    if _currentTemp == nil {
+      _currentTemp = 52.0
     }
-    
-    var currentTemp: Double {
-        if _currentTemp == nil {
-            _currentTemp = 52.0
+    return _currentTemp
+  }
+  
+  func downloadWeatherDetails(weatherUrl:String,completed: @escaping DownloadComplete) {
+    //Alamore fire download
+    Alamofire.request(weatherUrl).responseJSON { response in
+      let result = response.result
+      
+      if let dict = result.value as? Dictionary<String, AnyObject> {
+        if let name = dict["name"] as? String {
+          self._cityName = name.capitalized
+          print(self._cityName)
         }
-        return _currentTemp
-    }
-    
-    func downloadWeatherDetails(weatherUrl:String,completed: @escaping DownloadComplete) {
-        //Alamore fire download
-        Alamofire.request(weatherUrl).responseJSON { response in
-            let result = response.result
-            
-            if let dict = result.value as? Dictionary<String, AnyObject> {
-                if let name = dict["name"] as? String {
-                    self._cityName = name.capitalized
-                    print(self._cityName)
-                }
-                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
-                    if let main = weather[0]["main"] as? String {
-                        self._weatherType = main.capitalized
-                        print(self._weatherType)
-                    }
-                }
-                if let main = dict["main"] as? Dictionary<String, AnyObject> {
-                    if let currentTemperature = main["temp"] as? Double {
-                        let kelvinToFarenheitPreDivision = (currentTemperature * (9/5) - 459.67)
-                        let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision/10))
-                        self._currentTemp = kelvinToFarenheit
-                        print(self._currentTemp)
-                    }
-                }
-            }
-            completed()
+        if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+          if let main = weather[0]["main"] as? String {
+            self._weatherType = main.capitalized
+            print(self._weatherType)
+          }
         }
+        if let main = dict["main"] as? Dictionary<String, AnyObject> {
+          if let currentTemperature = main["temp"] as? Double {
+            let kelvinToFarenheitPreDivision = (currentTemperature * (9/5) - 459.67)
+            let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision/10))
+            self._currentTemp = kelvinToFarenheit
+            print(self._currentTemp)
+          }
+        }
+      }
+      completed()
     }
+  }
 }
--- MiWeather/WeatherVC.swift
+++ MiWeather/WeatherVC.swift
@@ -11,180 +11,180 @@ import CoreLocation
 import Alamofire
 
 class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
-    
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var currentTempLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var currentWeatherImage: UIImageView!
-    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    
-    let locationManager = CLLocationManager()
-    var currentLocation: CLLocation!
-    
-    var currentWeather: CurrentWeather = CurrentWeather()
-    var forecast: Forecast!
-    var forecasts = [Forecast]()
-    var urls :URLs!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        //locationManager.startUpdatingLocation()
-        locationManager.startMonitoringSignificantLocationChanges()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-     //   currentWeather = CurrentWeather()
-    //    currentWeather.downloadWeatherDetails {
-     //       self.downloadForecastData {
-      //          self.updateMainUI()
-      //      }
-      //  }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-       // locationAuthStatus()
-    }
-    
-    func locationAuthStatus(location:CLLocation) {
-            if (location != nil ) {
-                urls = URLs(withCoordinate: location.coordinate)
-                self.forecasts.removeAll()
-                currentWeather.downloadWeatherDetails(weatherUrl:urls.getWeatherUrl) {
-                    self.downloadForecastData(forcastUrl:self.urls.getForcastUrl) {
-                        self.updateMainUI()
-                    }
-                }
-            } else {
-                print("location : \(location) is nil ")
-        }
   
-}
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var currentTempLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var currentWeatherImage: UIImageView!
+  @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  
+  let locationManager = CLLocationManager()
+  var currentLocation: CLLocation!
+  
+  var currentWeather: CurrentWeather = CurrentWeather()
+  var forecast: Forecast!
+  var forecasts = [Forecast]()
+  var urls :URLs!
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-/*
-     func locationAuthStatus() {
-     //  print(locationManager.location?.coordinate)
-     // if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-     if (locationManager.location != nil ) {
-     print("here")
-     currentLocation = locationManager.location
-     Location.sharedInstance.latitude = currentLocation.coordinate.latitude
-     Location.sharedInstance.longitude = currentLocation.coordinate.longitude
-     currentWeather.downloadWeatherDetails {
-     self.downloadForecastData {
-     self.updateMainUI()
-     }
-     }
-     } else {
-     print("location manager: \(locationManager.location)")
-     }
-     // } else {
-     //   locationManager.requestWhenInUseAuthorization()
-     //    locationAuthStatus()
-     //}
-     }
-     */
-    func downloadForecastData(forcastUrl:String,completed: @escaping DownloadComplete) {
-        //Downloading forecast weather data for TableView
-        let forecastURL = URL(string: forcastUrl)!
-        Alamofire.request(forecastURL).responseJSON { response in
-            let result = response.result
-            
-            if let dict = result.value as? Dictionary<String, AnyObject> {
-                if let list = dict["list"] as? [Dictionary<String, AnyObject>] {
-                    
-                    for obj in list {
-                        let forecast = Forecast(weatherDict: obj)
-                        self.forecasts.append(forecast)
-                        print(obj)
-                    }
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                    //self.forecasts.remove(at: 0)
-                    //Data was blank so we had to reload data
-                   // self.tableView.reloadData()
-                }
-            }
-            completed()
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.requestWhenInUseAuthorization()
+    //locationManager.startUpdatingLocation()
+    locationManager.startMonitoringSignificantLocationChanges()
+    
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+    //   currentWeather = CurrentWeather()
+    //    currentWeather.downloadWeatherDetails {
+    //       self.downloadForecastData {
+    //          self.updateMainUI()
+    //      }
+    //  }
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    // locationAuthStatus()
+  }
+  
+  func locationAuthStatus(location:CLLocation) {
+    if (location != nil ) {
+      urls = URLs(withCoordinate: location.coordinate)
+      self.forecasts.removeAll()
+      currentWeather.downloadWeatherDetails(weatherUrl:urls.getWeatherUrl) {
+        self.downloadForecastData(forcastUrl:self.urls.getForcastUrl) {
+          self.updateMainUI()
         }
+      }
+    } else {
+      print("location : \(location) is nil ")
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let userLocation = manager.location else {
-            return
+  }
+  
+  /*
+   func locationAuthStatus() {
+   //  print(locationManager.location?.coordinate)
+   // if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+   if (locationManager.location != nil ) {
+   print("here")
+   currentLocation = locationManager.location
+   Location.sharedInstance.latitude = currentLocation.coordinate.latitude
+   Location.sharedInstance.longitude = currentLocation.coordinate.longitude
+   currentWeather.downloadWeatherDetails {
+   self.downloadForecastData {
+   self.updateMainUI()
+   }
+   }
+   } else {
+   print("location manager: \(locationManager.location)")
+   }
+   // } else {
+   //   locationManager.requestWhenInUseAuthorization()
+   //    locationAuthStatus()
+   //}
+   }
+   */
+  func downloadForecastData(forcastUrl:String,completed: @escaping DownloadComplete) {
+    //Downloading forecast weather data for TableView
+    let forecastURL = URL(string: forcastUrl)!
+    Alamofire.request(forecastURL).responseJSON { response in
+      let result = response.result
+      
+      if let dict = result.value as? Dictionary<String, AnyObject> {
+        if let list = dict["list"] as? [Dictionary<String, AnyObject>] {
+          
+          for obj in list {
+            let forecast = Forecast(weatherDict: obj)
+            self.forecasts.append(forecast)
+            print(obj)
+          }
+          DispatchQueue.main.async {
+            self.tableView.reloadData()
+          }
+          //self.forecasts.remove(at: 0)
+          //Data was blank so we had to reload data
+          // self.tableView.reloadData()
         }
-        print("lat: \(userLocation.coordinate.latitude) , lon:\(userLocation.coordinate.longitude)")
-        locationAuthStatus(location: userLocation)
+      }
+      completed()
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let userLocation = manager.location else {
+      return
+    }
+    print("lat: \(userLocation.coordinate.latitude) , lon:\(userLocation.coordinate.longitude)")
+    locationAuthStatus(location: userLocation)
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    print(status.rawValue)
+    if status == .denied || status == .restricted {
+      showAlert(title: "Go to your settings to enable location authorization for this app ;D")
+      manager.requestAlwaysAuthorization()
+      manager.requestWhenInUseAuthorization()
+    } else if status == .authorizedAlways || status == .authorizedWhenInUse {
+      //yay we have location
+      print("access")
+      locationManager.startUpdatingLocation()
+      // locationAuthStatus()
+    }
+  }
+  
+  // Should memorizes these delegates for tableview use
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return forecasts.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherCell
+    
+    if forecasts.count > indexPath.row + 1 {
+      let forecast = forecasts[indexPath.row]
+      cell.configureCell(forecast: forecast)
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-            print(status.rawValue)
-        if status == .denied || status == .restricted {
-            showAlert(title: "Go to your settings to enable location authorization for this app ;D")
-            manager.requestAlwaysAuthorization()
-            manager.requestWhenInUseAuthorization()
-        } else if status == .authorizedAlways || status == .authorizedWhenInUse {
-          //yay we have location
-           print("access")
-            locationManager.startUpdatingLocation()
-           // locationAuthStatus()
-        }
-    }
-
-    // Should memorizes these delegates for tableview use
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return forecasts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherCell
-        
-        if forecasts.count > indexPath.row + 1 {
-            let forecast = forecasts[indexPath.row]
-            cell.configureCell(forecast: forecast)
-        }
-     
-        return cell
-    }
-    
-    /*
-     if let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherCell {
-     
-     let forecast = forecasts[indexPath.row]
-     cell.configureCell(forecast: forecast)
-     return cell
-     } else {
-     return WeatherCell()
-     }
-     */
-    
-    func updateMainUI() {
-        dateLabel.text = currentWeather.date
-        currentTempLabel.text = "\(currentWeather.currentTemp)"
-        currentWeatherTypeLabel.text = currentWeather.weatherType
-        locationLabel.text = currentWeather.cityName
-        currentWeatherImage.image = UIImage(named: currentWeather.weatherType)
-    }
-    
-    func showAlert(title: String, message: String? = nil, style: UIAlertControllerStyle = .alert) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: dismissAlert)
-        alertController.addAction(okAction)
-        present(alertController, animated: true, completion: nil)
-    }
-    // Dimisses the alert
-    func dismissAlert(sender: UIAlertAction) {
-    }
+    return cell
+  }
+  
+  /*
+   if let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherCell {
+   
+   let forecast = forecasts[indexPath.row]
+   cell.configureCell(forecast: forecast)
+   return cell
+   } else {
+   return WeatherCell()
+   }
+   */
+  
+  func updateMainUI() {
+    dateLabel.text = currentWeather.date
+    currentTempLabel.text = "\(currentWeather.currentTemp)"
+    currentWeatherTypeLabel.text = currentWeather.weatherType
+    locationLabel.text = currentWeather.cityName
+    currentWeatherImage.image = UIImage(named: currentWeather.weatherType)
+  }
+  
+  func showAlert(title: String, message: String? = nil, style: UIAlertControllerStyle = .alert) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+    let okAction = UIAlertAction(title: "OK", style: .default, handler: dismissAlert)
+    alertController.addAction(okAction)
+    present(alertController, animated: true, completion: nil)
+  }
+  // Dimisses the alert
+  func dismissAlert(sender: UIAlertAction) {
+  }
 }
 
